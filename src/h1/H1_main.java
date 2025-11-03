08ffbaf745883f703aed72d138ac376db2cd14a8
sophier1507 <sophieroesgen@gmail.com>
2025-11-03 12:58:17 +0100

    h1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,36 @@
+package h1;
+
+public class H1_main {
+	public static void main(String[] args) {
+		
+		double guthaben, monEingang;
+		guthaben = -100;
+		monEingang = 200;
+		int rating = -2;
+		boolean warnhinweis, negativ;
+		warnhinweis = false;
+		negativ = true;
+		
+		if (guthaben < 0) {
+			negativ = true;
+			if (monEingang >= -guthaben) {
+				rating++;
+			} else {
+				rating--;
+			}
+		} else {
+			negativ = false;
+			if (guthaben > 0) {
+				rating += 3;
+			} else {
+				guthaben += 2;
+			}
+		}
+		if ((guthaben < 0) && (monEingang < -guthaben) && (rating < 0)) {
+			warnhinweis = true;
+		} else {
+			warnhinweis = false;
+		}
+		System.out.println("Negativ: " + negativ + "\nRating: " + rating + "\nWarnhinweis: " + warnhinweis);
+	}
+}
