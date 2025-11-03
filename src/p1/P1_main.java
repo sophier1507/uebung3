d4c1e8df5e16618f983cdc2a1159851eacd431c8
sophier1507 <sophieroesgen@gmail.com>
2025-11-03 12:39:03 +0100

    p1 Änderung

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -3,8 +3,8 @@ package p1;
 public class P1_main {
 
 	public static void main(String[] args) {
-		int wuerfel = 1;
-		if ((wuerfel == 1) || (wuerfel == 2)) {
+		int wuerfel = 4;
+		if ((wuerfel == 1) || (wuerfel == 4)) {
 			System.out.println("Gewonnen");
 		}
 		else if (wuerfel == 2) {
