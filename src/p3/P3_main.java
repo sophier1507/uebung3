adf229999de98b467944cb44591ff21411564dae
sophier1507 <sophieroesgen@gmail.com>
2025-11-03 12:33:59 +0100

    p3

---
+++ src/p3/P3_main.java
@@ -0,0 +1,21 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+		int a = -2;
+		if (a >= 0) {
+			System.out.println(a);
+		} else {
+			System.out.println(-a);
+		}
+		System.out.println(a*a);
+		if (a%2 == 0) {
+			System.out.println("Gerade");
+		} else {
+			System.out.println("Ungerade");
+		}
+
+	}
+
+}
